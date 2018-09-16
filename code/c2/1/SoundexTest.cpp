58899dc2aa59a3c1d7179581171bbb53f29ce778
Niklas <niklashaeuser@gmx.de>
2018-09-16 23:23:47 +0200

    Finished test RetainsSoleLetterOfOneLetterWord

--- code/c2/1/SoundexTest.cpp
+++ code/c2/1/SoundexTest.cpp
@@ -1,11 +1,22 @@
+//#include <iostream>
+#include <string>
+
 class Soundex {
 
+public:
+    std::string encode(const std::string& word) const {
+        return word;
+    }
 };
 
 
 #include <gmock/gmock.h>
+using ::testing::Eq;
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { 
    Soundex soundex;
-   ASSERT_FALSE(false);
+
+   auto encoded = soundex.encode("A");
+
+   ASSERT_THAT(encoded, Eq("A"));
 }
