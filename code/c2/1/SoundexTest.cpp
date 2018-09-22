062137d8863c25ce243600e6aa6577453ee2faa9
Niklas <niklashaeuser@gmx.de>
2018-09-22 17:27:24 +0200

    Add tests for non-alphabetical inputs and for multiple characters

--- code/c2/1/SoundexTest.cpp
+++ code/c2/1/SoundexTest.cpp
@@ -16,6 +16,14 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
     ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
 
-TEST_F(SoundexEncoding, ReplaceConsonantsWithAppropriateDigits) {
-    ASSERT_THAT(soundex.encode("AB"), Eq("A100"));
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+    ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+    ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesMultipleCharactewrsWithDigits) {
+    ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
 }
