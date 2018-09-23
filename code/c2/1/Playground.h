cf1276ae37db242305fd8c83a57fe26ec2dfba1d
Niklas <niklashaeuser@gmx.de>
2018-09-23 19:19:46 +0200

    Add Playground and test CombinesDuplicateEncodings

---
+++ code/c2/1/Playground.h
@@ -0,0 +1,19 @@
+//
+// Created by niklas on 23.09.18.
+//
+
+#ifndef CHAPTERFIRSTEXAMPLE_PLAYGROUND_H
+#define CHAPTERFIRSTEXAMPLE_PLAYGROUND_H
+
+
+#include <string>
+#include <iostream>
+
+
+void Playground() {
+
+    std::string word = "Abcd";
+    std::cout << "Playground Output: " << std::string(1, word.back()) << std::endl;
+}
+
+#endif //CHAPTERFIRSTEXAMPLE_PLAYGROUND_H
--- code/c2/1/main.cpp
+++ code/c2/1/main.cpp
@@ -1,7 +1,9 @@
 #include <gmock/gmock.h>
+#include "Playground.h"
 
 int main(int argc, char** argv) {
    testing::InitGoogleMock(&argc, argv);
+   Playground();
    return RUN_ALL_TESTS();
 }
 
